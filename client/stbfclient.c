f368dc3fdd5bf49e65421cd3dabf9bdb2c13dd6c
BoxuanXu <boxuan.xu@seetatech.com>
2018-04-09 20:02:39 +0800

    fix bug

--- client/stbfclient.c
+++ client/stbfclient.c
@@ -15,6 +15,7 @@
 #include <limits.h>
 #include "fdfs_client.h"
 #include "logger.h"
+#include "fdfs_global.h"
 
 int stbf_test() { return 0; }
 
@@ -54,12 +55,18 @@ int stbf_upload(const char* local_filename, char* ret_file_id) {
         //fdfs_client_destroy();
         return errno != 0 ? errno : ECONNREFUSED;
     }
+    pTrackerServer = tracker_get_connection();
+    if (pTrackerServer == NULL) {
+        fdfs_client_destroy();
+        return errno != 0 ? errno : ECONNREFUSED;
+    }
+
 
     *group_name = '\0';
     if ((result = tracker_query_storage_store(pTrackerServer, &storageServer,
                                               group_name, &store_path_index)) !=
         0) {
-        fdfs_client_destroy();
+        //fdfs_client_destroy();
         fprintf(stderr,
                 "tracker_query_storage fail, "
                 "error no: %d, error info: %s\n",
@@ -80,16 +87,21 @@ int stbf_upload(const char* local_filename, char* ret_file_id) {
     }
 
     tracker_disconnect_server(&storageServer);
+    tracker_disconnect_server(&trackerserver);
     tracker_disconnect_server(pTrackerServer);
-
+    //tracker_disconnect_server_ex(pTrackerServer, true);
     return result;
 }
 
 int stbf_download(const char* fdfs_id, char* local_path) {
+    //char group_name[FDFS_GROUP_NAME_MAX_LEN + 1] = {0};
     char* path_name = NULL;
     char* local_filename = NULL;
     ConnectionInfo* pTrackerServer;
+    //ConnectionInfo *pStorageServer;
     ConnectionInfo trackerserver;
+    //ConnectionInfo storageServer;
+    //int store_path_index = 0;
     int result = 0;
     int64_t file_size;
     int64_t file_offset;
@@ -106,6 +118,28 @@ int stbf_download(const char* fdfs_id, char* local_path) {
         return errno != 0 ? errno : ECONNREFUSED;
     }
 
+    /**group_name = '\0';
+    if ((result = tracker_query_storage_store(pTrackerServer, &storageServer,
+                                              group_name, &store_path_index)) !=
+        0) {
+        //fdfs_client_destroy();
+        fprintf(stderr,
+                "tracker_query_storage fail, "
+                "error no: %d, error info: %s\n",
+                result, STRERROR(result));
+        return result;
+    }
+
+    if((pStorageServer = tracker_connect_server(&storageServer, &result)) == NULL)
+    {
+        fprintf(stderr,
+                "tracker_connect_server to storage server fail, "
+                "error no: %d, error info: %s\n",
+                result, STRERROR(result));
+        return result;
+    }*/
+	
+
     file_offset = 0;
     download_bytes = 0;
     local_filename = strrchr(fdfs_id, '/');
@@ -114,19 +148,26 @@ int stbf_download(const char* fdfs_id, char* local_path) {
     } else {
         local_filename = (char*)(fdfs_id);
     }
-
+	
     path_name = malloc(PATH_MAX);
     sprintf(path_name, "%s/%s", local_path, local_filename);
     // SYZ, 20170704, local_filename -> path_name
-    result = storage_download_file_to_file1(pTrackerServer, NULL, fdfs_id, &path_name, &file_size);
+    result = storage_do_download_file1_ex(
+        pTrackerServer, NULL, FDFS_DOWNLOAD_TO_FILE, fdfs_id, file_offset,
+        download_bytes, &path_name, NULL, &file_size);
+    
+
+    //result = storage_download_file_to_file1(pTrackerServer, pStorageServer, fdfs_id, &path_name, &file_size);
     if (result != 0) {
         printf(
             "download file '%s' fail, "
             "error no: %d, error info: %s\n",
             local_filename, result, STRERROR(result));
     }
-    printf("%s\n", path_name);
+    //printf("%s\n", path_name);
 
+    //tracker_disconnect_server(pStorageServer);
+    tracker_disconnect_server(&trackerserver);
     tracker_disconnect_server(pTrackerServer);
 
     free(path_name);
@@ -157,6 +198,7 @@ int stbf_delete(const char* fdfs_id) {
             result, STRERROR(result));
     }
 
+    tracker_disconnect_server(&trackerserver);
     tracker_disconnect_server(pTrackerServer);
 
     return result;
